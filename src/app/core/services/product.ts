23d005c597d56c31326b04f0826fd7960b723578
abigail calenta <abicalenta@gmail.com>
2026-01-23 19:45:41 -0300

    new

--- src/app/core/services/product.ts
+++ src/app/core/services/product.ts
@@ -1,8 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Product {
-  
+  private api = 'https://w370351.ferozo.com/api/products';
+  constructor(private http: HttpClient) {}
+  getByRestaurant(id: number){
+    return this.http.get<any[]>(`${this.api}/restaurant/${id}`);
+    
+  }
+      getFeatured(id: number) {
+    return this.http.get<any[]>(`${this.api}/restaurant/${id}/featured`);
+  }
 }
+
+
--- src/app/pages/menu/menu.ts
+++ src/app/pages/menu/menu.ts
@@ -1,11 +1,1 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-menu',
-  imports: [],
-  templateUrl: './menu.html',
-  styleUrl: './menu.css',
-})
-export class Menu {
-
-}
